97640085afde2e666f05a55e6f4ddb01b9c31922
FerroEduardo <47820549+FerroEduardo@users.noreply.github.com>
2020-03-17 22:00:49 -0300

    1.1 Alguns gráficos de estatísticas do COVID-19 em alguns países são exibidos quando disponível

--- main.py
+++ main.py
@@ -1,13 +1,46 @@
 import os
 import csv
+import json
 import datetime
+import requests
+import plotly.graph_objects as go
 
+
+from lxml import html
 from pathlib import Path
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
+
+def abrirGraficoDoPaisPorNome(nomeDoPais, stats):
+
+
+    re = requests.get("https://restcountries.eu/rest/v2/name/"+nomeDoPais)
+    if "status" in re.text:
+        return
+    codigoDoPais = re.json()[0]["alpha2Code"]
+    i = 0
+    for key in stats:
+        if codigoDoPais in key:
+            # https://restcountries.eu/
+            req = requests.get("https://restcountries.eu/rest/v2/alpha?codes=" + key)
+            # print(req.json())
+            countryName = req.json()[0]["translations"]["br"]
+            # print(key, stats[key])
+            i += 1
+            fig = go.Figure()
+            fig.add_trace(go.Bar(y=stats[key][1], x=stats[key][0], name="Casos Confirmados", text=stats[key][5]))
+            fig.add_trace(go.Bar(y=stats[key][2], x=stats[key][0], name="Novos Casos Confirmados"))
+            fig.add_trace(go.Bar(y=stats[key][3], x=stats[key][0], name="Mortes"))
+            fig.add_trace(go.Bar(y=stats[key][4], x=stats[key][0], name="Novas Mortes"))
+            fig.update_layout(title='COVID-19 em ' + countryName,
+                              xaxis_title='Data',
+                              yaxis_title='Casos',
+                              template="plotly", )
+            fig.show()
 
 def main():
     url = "http://plataforma.saude.gov.br/novocoronavirus/"
@@ -142,21 +175,103 @@ def main():
         minuto = int(dateStr[5].split(":")[1])
         date = datetime.datetime(ano, mes, dia, hora, minuto)
         print("Países com com casos confirmados atualizados em " + date.strftime(
-            "%d/%m/%y, %H:%M") + "segundo a OMS. Fonte: http://plataforma.saude.gov.br/novocoronavirus/")
+            "%d/%m/%y, %H:%M") + " segundo a OMS. Fonte: http://plataforma.saude.gov.br/novocoronavirus/")
+
+        r = requests.get("http://plataforma.saude.gov.br/novocoronavirus/resources/scripts/database.js")
+        body = html.fromstring(r.content).text_content()
+
+        jsonBody = json.loads(body[13:])
+
+        stats = dict()
+        for key in jsonBody:
+            if key == "world":
+                for value in jsonBody[key]:
+                    for country in value['values']:
+
+                        if stats.get(country["uid"]):
+
+                            stats[country["uid"]][0].append(value["date"] + " , " + value["time"])
+
+                            if country.get("cases"):
+                                stats[country["uid"]][1].append(country["cases"])
+
+                            if country.get("casesNew"):
+                                stats[country["uid"]][2].append(country['casesNew'])
+                            else:
+                                stats[country["uid"]][2].append(0)
+
+                            if country.get('deaths'):
+                                stats[country["uid"]][3].append(country['deaths'])
+                            else:
+                                stats[country["uid"]][3].append(0)
+
+                            if country.get('deathsNew'):
+                                stats[country["uid"]][4].append(country['deathsNew'])
+                            else:
+                                stats[country["uid"]][4].append(0)
+
+                            if country.get('comments'):
+                                if len(stats[country["uid"]]) == 5:
+                                    stats[country["uid"]].append([country['comments']])
+                                else:
+                                    stats[country["uid"]][5].append([country['comments']])
+                            else:
+                                if len(stats[country["uid"]]) == 5:
+                                    stats[country["uid"]].append([""])
+                                else:
+                                    stats[country["uid"]][5].append("")
+
+                        else:
+                            stats[country["uid"]] = []
+
+                            stats[country["uid"]].append([value["date"] + " , " + value["time"]])
+
+                            if country.get("cases"):
+                                stats[country["uid"]].append([country.get("cases")])
+
+                            if country.get("casesNew"):
+                                stats[country["uid"]].append([country['casesNew']])
+                            else:
+                                stats[country["uid"]].append(["0"])
+
+                            if country.get('deaths'):
+                                stats[country["uid"]].append([country['deaths']])
+                            else:
+                                stats[country["uid"]].append([0])
+
+                            if country.get('deathsNew'):
+                                stats[country["uid"]].append([country['deathsNew']])
+                            else:
+                                stats[country["uid"]].append([0])
+
+                            if country.get('comments'):
+                                stats[country["uid"]].append([country['comments']])
+                            else:
+                                stats[country["uid"]].append([""])
+
         while True:
             if option.lower() == "nao":
                 break
 
             elif option.lower() == "todos":
+                # option = input("Você tem certeza que deseja abrir o gráfico de todos os países com casos do COVID-19? São, no total, " + str(len(paises)) + " (sim/nao) ")
+                # if option == "sim":
                 for pais in paises:
                     if pais["País"] is not None and pais["Óbitos"] is not None:
                         print(templateCountry.format(pais["País"],
                                                      pais["Casos confirmados"], pais["% Casos confirmados"],
                                                      pais["Taxa de letalidade******"], pais["Óbitos"]))
+                        # abrirGraficoDoPaisPorNome(pais["País"], stats)
+                # elif option == "nao":
+                #     print("")
+
                 break
 
             else:
                 name = input("Digite o nome do país:  ")
+
+                abrirGraficoDoPaisPorNome(name, stats)
+
                 try:
                     for pais in paises:
                         # print(country)
@@ -167,6 +282,7 @@ def main():
 
                 finally:
                     option = input("Deseja continuar(sim) ou sair(nao)? ")
+
 
     finally:
         driver.quit()
